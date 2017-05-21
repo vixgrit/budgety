40eabfe76f124932cc4d2253961c9bb17427f0ab
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-05-20 20:55:44 -0400

    moved class selectors into DOMstrings object

--- app.js
+++ app.js
@@ -6,14 +6,24 @@ var budgetController = (function() {
 //ui controller
 var UIController = (function() {
 
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  }
+
   return {
     getInput: function() {
       //will be either inc or exp
       return {
-        type: document.querySelector('.add__type').value,
-        description: document.querySelector('.add__description').value,
-        value: document.querySelector('.add__value').value
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
       }
+    },
+    getDOMstrings: function() {
+      return DOMstrings;
     }
   }
 
@@ -21,6 +31,8 @@ var UIController = (function() {
 
 //global app controller
 var controller = (function(budgetCtrl, UICtrl) {
+
+  var DOM = UICtrl.getDOMstrings();
 
   var ctrlAddItem = function() {
     //get field input data
@@ -35,7 +47,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     //display budget
   }
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
   document.addEventListener('keypress', function(event) {
     if (event.keyCode === 13 || event.which === 13) {
