0d1f104c262f2ccfd1a0881ac9587365a1367ee0
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-05-21 20:05:46 -0400

    create calculateBudget and getBudget funcs in budget controller

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ var budgetController = (function() {
     this.value = value;
   };
 
+  var calculateTotal = function(type) {
+    var sum = 0;
+    data.allItems[type].forEach(function(current) {
+      sum += current.value;
+    });
+    data.totals[type] = sum;
+  };
+
   var data = {
     allItems: {
       exp: [],
@@ -21,7 +29,9 @@ var budgetController = (function() {
     totals: {
       exp: 0,
       inc: 0
-    }
+    },
+    budget: 0,
+    percentage: -1
   };
 
   return {
@@ -45,9 +55,31 @@ var budgetController = (function() {
       data.allItems[type].push(newItem);
       //return new element
       return newItem;
+    },
+
+    calculateBudget: function() {
+      //calculate total income and expenses
+      calculateTotal('exp');
+      calculateTotal('inc');
+      //calculate budget: income - expenses
+      data.budget = data.totals.inc - data.totals.exp;
+      //calculate % of income that we spent
+      if (data.totals.inc > 0) {
+        data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+      }
+      else {
+        data.percentage = -1;
+      }
+    },
+    getBudget: function() {
+      return {
+        budget: data.budget,
+        totalInc: data.totals.inc,
+        totalExp: data.totals.exp,
+        percentage: data.percentage
+      };
     }
   }
-
 })();
 
 //ui controller
@@ -124,10 +156,11 @@ var controller = (function(budgetCtrl, UICtrl) {
 
   var updateBudget = function() {
     //calculate budget
-
+    budgetCtrl.calculateBudget();
     //return budget
-
+    var budget = budgetCtrl.getBudget();
     //display budget
+    console.log(budget);
   };
 
   var ctrlAddItem = function() {
