8083f5f0bd513e7dfe96ae68b7179d838b758508
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-05-22 15:36:19 -0400

    init calls default displayBudget w/ prop values of zero

--- app.js
+++ app.js
@@ -91,7 +91,11 @@ var UIController = (function() {
     inputValue: '.add__value',
     inputBtn: '.add__btn',
     incomeContainer: '.income__list',
-    expensesContainer: '.expenses__list'
+    expensesContainer: '.expenses__list',
+    budgetLabel: '.budget__value',
+    incomeLabel: '.budget__income--value',
+    expensesLabel: '.budget__expenses--value',
+    percentageLabel: '.budget__expenses--percentage'
   }
 
   return {
@@ -132,6 +136,17 @@ var UIController = (function() {
       });
       fieldsArr[0].focus();
     },
+    displayBudget: function(obj) {
+      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+      document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+      if (obj.percentage > 0) {
+        document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%'
+      }
+      else {
+        document.querySelector(DOMstrings.percentageLabel).textContent = '---'
+      }
+    },
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -160,7 +175,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     //return budget
     var budget = budgetCtrl.getBudget();
     //display budget
-    console.log(budget);
+    UICtrl.displayBudget(budget);
   };
 
   var ctrlAddItem = function() {
@@ -182,6 +197,12 @@ var controller = (function(budgetCtrl, UICtrl) {
 
   return {
     init: function() {
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: 0
+      });
       setupEventListeners();
     }
   }
