39ce0564b98d127b1f47baa49c8411bf790c5196
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-05-23 01:48:20 -0400

    update percentages in budgetController

--- app.js
+++ app.js
@@ -5,6 +5,21 @@ var budgetController = (function() {
     this.id = id;
     this.description = description;
     this.value = value;
+    this.percentage = -1;
+  };
+
+  Expense.prototype.calcPercentage = function(totalIncome) {
+    if (totalIncome > 0) {
+      this.percentage = Math.round((this.value / totalIncome) * 100);
+
+    }
+    else {
+      this.percentage = -1;
+    }
+  };
+
+  Expense.prototype.getPercentage = function() {
+    return this.percentage;
   };
 
   var Income  = function(id, description, value) {
@@ -81,6 +96,17 @@ var budgetController = (function() {
       else {
         data.percentage = -1;
       }
+    },
+    calculatePercentages: function() {
+      data.allItems.exp.forEach(function(cur) {
+        cur.calcPercentage(data.totals.inc);
+      });
+    },
+    getPercentages: function() {
+      var allPerc = data.allItems.exp.map(function(cur) {
+        return cur.getPercentage();
+      });
+      return allPerc;
     },
     getBudget: function() {
       return {
@@ -200,6 +226,15 @@ var controller = (function(budgetCtrl, UICtrl) {
     UICtrl.displayBudget(budget);
   };
 
+  var updatePercentages = function() {
+    //calculate percentages
+    budgetCtrl.calculatePercentages();
+    //read percentages from budget controller
+    var percentages = budgetCtrl.getPercentages();
+    //update UI with new percentages
+    console.log(percentages);
+  };
+
   var ctrlAddItem = function() {
     var input, newItem
     //get field input data
@@ -214,6 +249,8 @@ var controller = (function(budgetCtrl, UICtrl) {
       UICtrl.clearFields();
       //calculate and update budget
       updateBudget();
+      //calculate and update percentages
+      updatePercentages();
     }
   };
 
@@ -231,6 +268,8 @@ var controller = (function(budgetCtrl, UICtrl) {
       UICtrl.deleteListItem(itemID);
 
       updateBudget();
+
+      updatePercentages();
     }
   };
 
