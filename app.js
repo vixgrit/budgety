33ee9914094277ee5b754350a2d084777f819d37
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-05-23 02:57:44 -0400

    display current month and year

--- app.js
+++ app.js
@@ -137,7 +137,8 @@ var UIController = (function() {
     expensesLabel: '.budget__expenses--value',
     percentageLabel: '.budget__expenses--percentage',
     container: '.container',
-    expensesPercLabel: '.item__percentage'
+    expensesPercLabel: '.item__percentage',
+    dateLabel: '.budget__title--month'
   };
 
   var formatNumber = function(num, type) {
@@ -230,6 +231,16 @@ var UIController = (function() {
         }
       });
     },
+    displayMonth: function() {
+      var now, months, month, year;
+      now = new Date();
+      months = [
+        'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
+      ];
+      month = now.getMonth();
+      year = now.getFullYear();
+      document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+    },
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -312,6 +323,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 
   return {
     init: function() {
+      UICtrl.displayMonth();
       UICtrl.displayBudget({
         budget: 0,
         totalInc: 0,
