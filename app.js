4f04042a8f73066977a9a27b8eceefe99881bbef
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-05-23 02:24:59 -0400

    update percentages in UI controller

--- app.js
+++ app.js
@@ -136,7 +136,8 @@ var UIController = (function() {
     incomeLabel: '.budget__income--value',
     expensesLabel: '.budget__expenses--value',
     percentageLabel: '.budget__expenses--percentage',
-    container: '.container'
+    container: '.container',
+    expensesPercLabel: '.item__percentage'
   }
 
   return {
@@ -193,6 +194,24 @@ var UIController = (function() {
         document.querySelector(DOMstrings.percentageLabel).textContent = '---'
       }
     },
+    displayPercentages: function(percentages) {
+      var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+
+      var nodeListForEach = function(list, callback) {
+        for (var i = 0; i < list.length; i++) {
+          callback(list[i], i);
+        }
+      };
+
+      nodeListForEach(fields, function(current, index) {
+        if (percentages[index] > 0) {
+          current.textContent = percentages[index] + '%';
+        }
+        else {
+          current.textContent = '---';
+        }
+      });
+    },
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -232,7 +251,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     //read percentages from budget controller
     var percentages = budgetCtrl.getPercentages();
     //update UI with new percentages
-    console.log(percentages);
+    UICtrl.displayPercentages(percentages);
   };
 
   var ctrlAddItem = function() {
