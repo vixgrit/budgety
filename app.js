f751e9a1ef8a7b82b7185780c75556eb0196475a
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-05-23 01:17:41 -0400

    handler for deleting item from UI

--- app.js
+++ app.js
@@ -57,6 +57,17 @@ var budgetController = (function() {
       return newItem;
     },
 
+    deleteItem: function(type, id) {
+      var ids, index;
+      var ids = data.allItems[type].map(function(current) {
+        return current.id;
+      });
+      index = ids.indexOf(id);
+      if (index !== -1) {
+        data.allItems[type].splice(index, 1);
+      }
+    },
+
     calculateBudget: function() {
       //calculate total income and expenses
       calculateTotal('exp');
@@ -78,6 +89,9 @@ var budgetController = (function() {
         totalExp: data.totals.exp,
         percentage: data.percentage
       };
+    },
+    testing: function() {
+      return data;
     }
   }
 })();
@@ -126,6 +140,11 @@ var UIController = (function() {
 
       //insert html into DOM
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+    },
+
+    deleteListItem: function(selectorID) {
+      var el = document.getElementById(selectorID);
+      el.parentNode.removeChild(el);
     },
 
     clearFields: function() {
@@ -204,8 +223,14 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     if (itemID) {
       splitID = itemID.split('-');
-      type = splitID = 0;
-      ID = splitID[1];
+      type = splitID[0];
+      ID = parseInt(splitID[1]);
+
+      budgetCtrl.deleteItem(type, ID);
+
+      UICtrl.deleteListItem(itemID);
+
+      updateBudget();
     }
   };
 
